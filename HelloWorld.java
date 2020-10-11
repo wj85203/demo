eed8bc8b3f57efeb19d9b1d3c346f09eea784134
wj852033 <=>
2020-10-11 17:07:23 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public void HelloWorld {
+	public statis void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println(1);
+		System.out.println("Hello World");
+		System.out.println(2);
+	}
+}
